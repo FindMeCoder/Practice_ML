fb95cf9851912ff5e5b4cec3184898c5be8c075e
FindMe <18640144558@163.com>
2018-10-08 10:39:37 +0800

    完成ROC曲线的绘制

--- 7/adaboost.py
+++ 7/adaboost.py
@@ -80,7 +80,7 @@ def adaBoostTrainDS(dataArr,classLabels,numIt=40): #使用单层决策树构建
         print("total error rate: is %d"%(errorRate))
         if errorRate==0.0:
             break
-    return weakClassArr
+    return weakClassArr,aggClassEst
 
 #测试算法，基于AdaBoost的分类
     
@@ -92,7 +92,7 @@ def adaClassify(datToClass,classifierArr):
         classEst=stumpClassify(dataMatrix,classifierArr[i]['dim'],classifierArr[i]['thresh'],\
                                classifierArr[i]['ineq'])
         aggClassEst+=classifierArr[i]['alpha']*classEst
-        print(aggClassEst)
+       # print(aggClassEst)
     return sign(aggClassEst)
 
 #在一个数据集上使用AdaBoost
@@ -110,96 +110,38 @@ def loadDataSet(fileName):
         labelMat.append(float(currLine[-1]))
     return dataMat,labelMat
 
-
+#其他分类性能度量指标：正确率召回率以及roc曲线
     
+#混淆矩阵
+#正确率=TP/(TP+FP) 召回率表示预测正确的正例占所有正例的比例TP/(TP+FN)
+#模型应该保持正确率和召回率尽可能的大
+#另一个度量分类中的非均衡的工具是ROC曲线，包括两条曲线，横轴为假阳率，横轴为真正率
     
+#roc曲线的绘制及AUC计算函数
+#predStrength为分类器预测强度
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    
-                
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def plotROC(predStrengths,classLabels):
+    import matplotlib.pyplot as plt
+    cur=(1.0,1.0)
+    ySum=0.0
+    numPosClas=sum(array(classLabels)==1.0)
+    xStep=1/float(len(classLabels)-numPosClas);yStep=1/float(numPosClas);
+    sortedIndicies=predStrengths.argsort()
+    fig=plt.figure()
+    fig.clf()
+    ax=plt.subplot(111)
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index]==1.0:
+            delX=0;delY=yStep;
+        else:
+            delX=xStep;delY=0;
+            ySum+=cur[1]
+        ax.plot([cur[0],cur[0]-delX],[cur[1],cur[1]-delY],c='b')
+        cur=(cur[0]-delX,cur[1]-delY)
+    ax.plot([0,1],[0,1],'b--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Postive Rate')
+    plt.title('ROC curve for AdaBoost Horse Detection System')
+    ax.axis([0,1,0,1])
+    plt.show()
+    print("The Area under the Curve is %f"%(ySum*xStep))
